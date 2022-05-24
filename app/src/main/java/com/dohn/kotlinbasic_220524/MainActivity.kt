cb00caa8366327f6cc4df300dfe30094229afd82
강도훈 <105531571+donishoon@users.noreply.github.com>
2022-05-24 22:08:47 +0900

    로그버튼이 눌리면 > 로그 찍어보기

--- app/src/main/java/com/dohn/kotlinbasic_220524/MainActivity.kt
+++ app/src/main/java/com/dohn/kotlinbasic_220524/MainActivity.kt
@@ -2,6 +2,7 @@ package com.dohn.kotlinbasic_220524
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
 
         btnLog.setOnClickListener {
 
+
+            Log.d( "메인화면", "로그 버튼 눌림" )
 
 
 
