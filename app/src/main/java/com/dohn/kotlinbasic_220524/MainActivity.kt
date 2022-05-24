6e88f4c83ba61039f5ebacade2058651d95d5fea
강도훈 <105531571+donishoon@users.noreply.github.com>
2022-05-24 22:42:17 +0900

    변수 문법 연습중 - 변수 생성 / 값 대입

--- app/src/main/java/com/dohn/kotlinbasic_220524/MainActivity.kt
+++ app/src/main/java/com/dohn/kotlinbasic_220524/MainActivity.kt
@@ -23,7 +23,29 @@ class MainActivity : AppCompatActivity() {
         btnToast.setOnClickListener {
 
 
-            Toast.makeText( this, "이건 알람이다" , Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "이건알람이다", Toast.LENGTH_SHORT).show()
+
+
+
+        }
+
+
+        btnVariable.setOnClickListener {
+
+//          여기는 변수 문법 연습 (주석 - Ctrl + / )
+
+
+//           var / val로 기록 공간(변수) 만들기
+            var name1 : String
+            val name2 : String
+
+//            두개의 이름 각각 저장하기 (메모 기록하기)
+
+            name1 = "강도훈"
+            name2 = "강도치"
+
+            name1 = "박철수"
+//            name2 = "끙아"
 
 
         }
